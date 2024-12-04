f4c364bbda8c5f8b6e23a0d5c551f1f9b326262e
Robin van der Noord <robinvandernoord@gmail.com>
2024-12-04 08:45:30 +0100

    day 4: pt2

--- day_4/main.rs
+++ day_4/main.rs
@@ -4,81 +4,67 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use anyhow::bail;
 
 type Coordinate = (isize, isize);
 type Matrix = HashMap<Coordinate, char>;
 
 const TARGET: &str = "XMAS";
 
-
 fn char_at<'matrix>(matrix: &'matrix Matrix, coord: &Coordinate) -> &'matrix char {
-    matrix.get(&coord).unwrap_or(&'_')
+    matrix.get(coord).unwrap_or(&'_')
 }
 
-fn try_find_xmas(
-    coordinate: &Coordinate,
-    matrix: &Matrix,
-    d_x: isize,
-    d_y: isize,
-) -> bool {
-    let mut coord = coordinate.clone();
+fn try_find_xmas(coordinate: &Coordinate, matrix: &Matrix, d_x: isize, d_y: isize) -> bool {
+    let mut coord = *coordinate;
 
     for char in TARGET.chars() {
         if char_at(matrix, &coord) != &char {
-            return false
+            return false;
         }
 
         coord = (coord.0 + d_x, coord.1 + d_y)
-
     }
 
     true
 }
 
-
-fn try_find_mas(
-    coordinate: &Coordinate,
-    matrix: &Matrix,
-    d_x: isize,
-    d_y: isize,
-) -> bool {
+fn try_find_mas(coordinate: &Coordinate, matrix: &Matrix, d_x: isize, d_y: isize) -> bool {
     // 1 check that coord is A
     // 2 check that coord +dx,dy = M
     // 3 check that coord -dx,dy = S
-    let mut coord = coordinate.clone();
+    let mut coord = *coordinate;
 
     // 1
     if char_at(matrix, &coord) != &'A' {
-        return false
+        return false;
     }
 
     // 2
-    coord = (coordinate.0 + d_x, coordinate.0 + d_y);
+    coord = (coordinate.0 + d_x, coordinate.1 + d_y);
     if char_at(matrix, &coord) != &'M' {
-        return false
+        return false;
     }
 
     // 3
-    coord = (coordinate.0 - d_x, coordinate.0 - d_y);
+    coord = (coordinate.0 - d_x, coordinate.1 - d_y);
     if char_at(matrix, &coord) != &'S' {
-        return false
+        return false;
     }
 
     true
 }
 
-fn try_find_mas_x(
-    coordinate: &Coordinate,
-    matrix: &Matrix,
-) -> bool {
+fn try_find_mas_x(coordinate: &Coordinate, matrix: &Matrix) -> bool {
     // find MAS on one diagonal AND on the other
     let mas_1 = try_find_mas(coordinate, matrix, -1, -1) | try_find_mas(coordinate, matrix, 1, 1);
     let mas_2 = try_find_mas(coordinate, matrix, -1, 1) | try_find_mas(coordinate, matrix, 1, -1);
 
-    mas_1 & mas_2
-}
+    // find MAS on horizontal and vertical:
+    // let mas_3 = try_find_mas(coordinate, matrix, -1, 0) | try_find_mas(coordinate, matrix, 1, 0);
+    // let mas_4 = try_find_mas(coordinate, matrix, 0, -1) | try_find_mas(coordinate, matrix, 0, 1);
 
+    mas_1 & mas_2 // | (mas_3 & mas_4)
+}
 
 async fn simple(file: FileHandle) -> anyhow::Result<i64> {
     let mut x_es: Vec<Coordinate> = vec![];
@@ -96,9 +82,8 @@ async fn simple(file: FileHandle) -> anyhow::Result<i64> {
 
     let mut score = 0;
     for coordinate in &x_es {
-
-        for d_x in -1 ..= 1 {
-            for d_y in -1 ..= 1 {
+        for d_x in -1..=1 {
+            for d_y in -1..=1 {
                 if try_find_xmas(coordinate, &matrix, d_x, d_y) {
                     score += 1
                 }
@@ -127,7 +112,8 @@ async fn advanced(file: FileHandle) -> anyhow::Result<i64> {
     for coordinate in &a_coords {
         if try_find_mas_x(coordinate, &matrix) {
             score += 1
-        } }
+        }
+    }
 
     Ok(score)
 }
@@ -174,9 +160,21 @@ async fn test_advanced_minimal() {
     assert_eq!(advanced(file).await.expect("Oof 1"), answer);
 }
 
+// #[tokio::test]
+// async fn test_advanced_shape2() {
+//     // with MAS in a + shape instead of x
+//     let answer = 1;
+//
+//     let file = read_lines("shape_two.txt")
+//         .await
+//         .expect("Should be able to read minimal.txt");
+//
+//     assert_eq!(advanced(file).await.expect("Oof 1"), answer);
+// }
+
 #[tokio::test]
 async fn test_advanced() {
-    let answer = 0;
+    let answer = 1809;
 
     let file = read_lines("simple.txt")
         .await
@@ -184,7 +182,10 @@ async fn test_advanced() {
 
     let result = advanced(file).await.expect("Oof 2");
 
+    dbg!(result);
+
     assert!(result > 441); // 441 too low
+    assert!(result < 1840); // 1840 too high
 
     assert_eq!(result, answer);
 }
